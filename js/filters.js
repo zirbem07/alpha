7d99b61440c3fc562ae09da8c938f261096e9152
sean <baumgs86@uwosh.edu>
2014-12-17 23:01:07 -0600

    anotherMerge

--- js/filters.js
+++ js/filters.js
@@ -3,4 +3,9 @@ angular.module('HCAlpha.filters',[])
         return function(items) {
             return items.slice().reverse();
         };
+    })
+    .filter('capitalize', function() {
+        return function(input, all) {
+            return (!!input) ? input.replace(/([^\W_]+[^\s-]*) */g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
+        }
     });
